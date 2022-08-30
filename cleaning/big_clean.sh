2ee446febbd51769bbaac0ef21d785137737a184
burde041 <burde041@morris.umn.edu>
2022-08-30 15:32:51 -0500

    big_clean deletes all files containing DELETE ME!

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -9,3 +9,6 @@ tar -xzf $1 --directory $TMPDIR
 #Changes to extracted directory
 argname=$(basename $1 .tgz)
 cd $TMPDIR/$argname
+
+#Deletes all files containing DELETE ME!
+grep -l 'DELETE ME!' * | xargs rm -f
