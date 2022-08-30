50b813d636d749db621d73a7281657e73f08868b
burde041 <burde041@morris.umn.edu>
2022-08-30 15:27:13 -0500

    extract_and_compile runs C program with a given argument

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -3,8 +3,13 @@
 # Compiles C program and runs it
 # Requires an argument that will be passed to the C program
 
+# Uncompress and extract contents of tar archive
 tar -xzf NthPrime.tgz
 
 cd NthPrime
 
+# Compiles C program
 gcc main.c nth_prime.c -o NthPrime
+
+# Runs C program
+./NthPrime $1
