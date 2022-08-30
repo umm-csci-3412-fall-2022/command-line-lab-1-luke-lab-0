56226bacf678aecc336f9bb6a2a1de586d350182
burde041 <burde041@morris.umn.edu>
2022-08-30 15:36:48 -0500

    Modify big_clean to compile with shellcheck

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -4,14 +4,14 @@
 TMPDIR=$(mktemp -d)
 
 #Extracts archive to temp directory 
-tar -xzf $1 --directory $TMPDIR
+tar -xzf "$1" --directory "$TMPDIR"
 
 #Changes to extracted directory
-argname=$(basename $1 .tgz)
-cd $TMPDIR/$argname
+argname=$(basename "$1" .tgz)
+cd "$TMPDIR/$argname" || exit
 
 #Deletes all files containing DELETE ME!
-grep -l 'DELETE ME!' * | xargs rm -f
+grep -l 'DELETE ME!' ./* | xargs rm -f
 
 #Creates a new cleaned archive
-tar czf "$OLDPWD"/cleaned_"$1" -C ./.. $argname
+tar czf "$OLDPWD"/cleaned_"$1" -C ./.. "$argname"
